34b6be9e3683f665b6ec2b09b421919ec16c4d65
Olivier <olivier.atesyan@ieseg.fr>
2020-11-01 15:38:10 +0100

    add ssqlite package

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'djirave24',
+        api_key: '782777953773998',
+        api_secret: '4_pTf5UgOvnN-UEAO90tv4HE_Es',
+      },
+    },
+  });
